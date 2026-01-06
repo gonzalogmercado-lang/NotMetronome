9cbb0b27ac9c1b60bdd30317d5bff5dd7389ffc2
Gonzalo Mercado <TU_MAIL@algo.com>
2026-01-06 16:08:39 -0300

    chore: use safe-area-context SafeAreaView

--- src/app/AppRoot.tsx
+++ src/app/AppRoot.tsx
@@ -1,10 +1,12 @@
-import React from "react";
-import { Text, View } from "react-native";
+import { Text } from "react-native";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 export default function AppRoot() {
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text style={{ fontSize: 24 }}>NotMetronome OK ✅</Text>
-    </View>
+    <SafeAreaProvider>
+      <SafeAreaView style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <Text>NotMetronome OK ✅</Text>
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
