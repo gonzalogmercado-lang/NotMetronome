8e5a17eae6dbf7b6f5aea8026176aea494923196
Gonzalo Mercado <TU_MAIL@algo.com>
2026-01-19 12:32:03 -0300

    Remove Creator tab from bottom navigation

--- src/navigation/TabsNavigator.tsx
+++ src/navigation/TabsNavigator.tsx
@@ -1,12 +1,10 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
-import CreatorScreen from "../screens/Creator/CreatorScreen";
 import HomeScreen from "../screens/Home/HomeScreen";
 import SettingsScreen from "../screens/Settings/SettingsScreen";
 
 export type TabsParamList = {
   Home: undefined;
-  Creator: undefined;
   Settings: undefined;
 };
 
@@ -14,9 +12,8 @@ const Tab = createBottomTabNavigator<TabsParamList>();
 
 function TabsNavigator() {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator id="root-tabs">
       <Tab.Screen name="Home" component={HomeScreen} />
-      <Tab.Screen name="Creator" component={CreatorScreen} />
       <Tab.Screen name="Settings" component={SettingsScreen} />
     </Tab.Navigator>
   );
